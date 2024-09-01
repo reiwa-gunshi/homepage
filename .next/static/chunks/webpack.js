f7e03a4f7dc45c7b365b3b440f090a31c0ec67bd
Hiroshi-create <hi130404@gmail.com>
2024-09-02 05:35:28 +0900

    サブタイトルの見切れ調整

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "610f11f9a84fe276"; }
+/******/ 		__webpack_require__.h = function() { return "e44fbeae76029173"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
