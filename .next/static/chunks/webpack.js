d1ef8b2704e8ac2ab712b378c24e8329f713e377
Hiroshi-create <hi130404@gmail.com>
2024-09-02 04:34:01 +0900

    サブタイトルの見切れ解消

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2143f41625a30fdb"; }
+/******/ 		__webpack_require__.h = function() { return "a4122fbaeba3ec7f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
