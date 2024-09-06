63c5ecd668624a0b88d36a7132ff3a31d3748caf
Hiroshi-create <hi130404@gmail.com>
2024-09-06 14:04:31 +0900

    footer２行にならないように

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0f414704ca8446b0"; }
+/******/ 		__webpack_require__.h = function() { return "f20fcb30c8644899"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
