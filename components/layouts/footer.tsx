5d51f80478c16626499b74ee17d6069e991e291c
Hiroshi-create <hi130404@gmail.com>
2024-09-09 11:46:49 +0900

    軍師プランのリンク変更

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -42,7 +42,7 @@ const Footer = (props: { nav_items: nav_type[] }) => {
               <p>『軍師プラン』</p>
             </div>
             <div className={styles.btn}>
-              <Link href="https://movedoor.sharepoint.com/sites/sharefolder/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2Fsharefolder%2FShared%20Documents%2F09%2EMD%E3%83%97%E3%83%AC%E3%82%BC%E3%83%B3%E8%B3%87%E6%96%99%2F%E8%BB%8D%E5%B8%AB%E3%83%95%E3%82%9A%E3%83%A9%E3%83%B3%EF%BC%9A%E3%81%93%E3%82%99%E8%AA%AC%E6%98%8E%E8%B3%87%E6%96%99%2Epdf&parent=%2Fsites%2Fsharefolder%2FShared%20Documents%2F09%2EMD%E3%83%97%E3%83%AC%E3%82%BC%E3%83%B3%E8%B3%87%E6%96%99&p=true&ga=1">
+              <Link href="https://movedoor.sharepoint.com/:b:/s/sharefolder/ET79mo8YuA1Kk6GJKUxj6GoBfFTlXFiJdh1aai-dq4cNbg?e=V8Jie4">
                 <a>
                   <div className={styles.rect_btn}>
                     <div className={styles.inner}>download</div>
